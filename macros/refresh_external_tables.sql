c0e6a478acfa891c3cb51ef9c743a4cca681f7bb
Alexandro Hou <alexandro.hou@tamadia.ch>
2024-05-06 17:40:31 +0100

    new test

--- macros/refresh_external_tables.sql
+++ macros/refresh_external_tables.sql
@@ -9,10 +9,10 @@
             {%- endset %}
             {{ return([ddl]) }}
         {% else %}
+            {# https://docs.aws.amazon.com/athena/latest/ug/alter-table-add-partition.html #}
+            {%- set part_len = partitions|length -%}
             {%- set finals = [] -%}
             {%- for partition in partitions -%}
-                {%- set partition_parts = [] -%}
-                {%- set path_parts = [] -%}
                 {%- if partition.vals.macro -%}
                     {%- set vals = dbt_external_tables.render_from_context(partition.vals.macro, **partition.vals.args) -%}
                 {%- elif partition.vals is string -%}
@@ -20,18 +20,15 @@
                 {%- else -%}
                     {%- set vals = partition.vals -%}
                 {%- endif -%}
-                {%- for val in vals %}
-                    {%- do partition_parts.append({
-                        'name': partition.name,
-                        'value': val
-                    }) -%}
-                    {%- do path_parts.append(dbt_external_tables.render_from_context(partition.path_macro, partition.name, val)) -%}
+                {%- for val in vals -%}
+                    {%- set partition_parts = [{'name': partition.name, 'value': val}] -%}
+                    {%- set path_parts = [dbt_external_tables.render_from_context(partition.path_macro, partition.name, val)] -%}
+                    {%- set construct = {
+                        'partition_by': partition_parts,
+                        'path': path_parts | join('/')
+                    } -%}
+                    {% do finals.append(construct) %}
                 {%- endfor -%}
-                {%- set construct = {
-                    'partition_by': partition_parts,
-                    'path': path_parts | join('/')
-                } -%}
-                {%- do finals.append(construct) %}
             {%- endfor -%}
             {%- set ddl = dbt_external_tables.redshift_alter_table_add_partitions(source_node, finals) -%}
             {{ return(ddl) }}
